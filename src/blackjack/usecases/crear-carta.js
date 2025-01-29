f6595d9f6a89074806081288a3d6f83bf575582e
Alejandro Rafael Fernandez Ramirez <a2010fernandez@outlook.com>
2025-01-29 16:54:41 -0500

    final

--- src/blackjack/usecases/crear-carta.js
+++ src/blackjack/usecases/crear-carta.js
@@ -13,7 +13,7 @@ export const crearCarta = (carta, turno) => {
     const divCartasJugadores = document.querySelectorAll('.divCartas')
  
     const imgCarta = document.createElement('img');
-    imgCarta.src = `/assets/cartas/${carta}.png` 
+    imgCarta.src = `/new-blackjack/assets/cartas/${carta}.png` 
     imgCarta.classList.add('carta');
     divCartasJugadores[turno].append(imgCarta)
 }
